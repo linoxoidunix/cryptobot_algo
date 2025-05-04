78baf34746bc583430f5ab46dae584eba35da7fa
Denis Evlanov <deniska-evlanov@mail.ru>
2025-05-04 21:14:53 +0300

    Refactor Bybit order book implementation and add example31
    - Comment out execution response listener implementation in listener_default.h
    - Add new example31 directory in CMakeLists.txt
    - Simplify main.cpp in example31 to focus on order book subscription:
      - Remove order manager and execution channel related code
      - Implement basic order book channel setup with subscription builder
      - Use TradingPairPrinter for order book data

--- examples/example31/main.cpp
+++ examples/example31/main.cpp
@@ -10,148 +10,8 @@
 #include <thread>
 
 #include "aoe/aoe.h"
-#include "aoe/bybit/execution_watcher/execution_watcher.h"
-#include "aoe/bybit/response_queue_listener/json/ws/execution_response/listener_default.h"
-#include "aoe/bybit/response_queue_listener/json/ws/order_response/listener_default.h"
 #include "aos/aos.h"
-#include "aos/position/position.h"
-#include "aos/position_storage/position_storage_by_pair/position_storage_by_pair_default.h"
-#include "aos/trading_pair_printer/trading_pair_printer.h"
 #include "aot/common/mem_pool.h"
-
-class BybitOrderManagerListener {
-  public:
-    using WebSocketSessionW = aoe::bybit::WebSocketPrivateSessionWInterface;
-    using ResponseQueue     = moodycamel::ConcurrentQueue<std::vector<char>>;
-
-  private:
-    boost::asio::thread_pool& thread_pool_;
-    WebSocketSessionW& session_ref_;
-
-    aoe::impl::WebSocketSessionProvider<common::MemoryPoolThreadSafety>
-        wss_provider_;
-    aoe::bybit::impl::external::ws::SingleOrderAPIDefault<
-        common::MemoryPoolThreadSafety>
-        bybit_api_;
-    aos::impl::MultiOrderManagerDefault<common::MemoryPoolThreadSafety>
-        multi_order_manager_;
-    std::unique_ptr<
-        aoe::bybit::impl::OrderManagerDefault<common::MemoryPoolThreadSafety>>
-        order_manager_;
-    aoe::bybit::impl::OrderWatcher<common::MemoryPoolThreadSafety>
-        order_watcher_;
-
-    ResponseQueue response_queue_;
-    aoe::bybit::impl::order_response::ListenerDefault<
-        common::MemoryPoolThreadSafety>
-        listener_;
-
-  public:
-    BybitOrderManagerListener(boost::asio::thread_pool& thread_pool,
-                              WebSocketSessionW& session)
-        : thread_pool_(thread_pool),
-          session_ref_(session),
-          wss_provider_(session_ref_),
-          bybit_api_(wss_provider_),
-          order_manager_(std::make_unique<aoe::bybit::impl::OrderManagerDefault<
-                             common::MemoryPoolThreadSafety>>(bybit_api_)),
-          order_watcher_(*order_manager_),
-          listener_(thread_pool_, response_queue_, order_watcher_, 100) {
-        multi_order_manager_.Register(common::ExchangeId::kBybit,
-                                      std::move(order_manager_));
-    }
-    aoe::ResponseQueueListenerInterface& GetListener() { return listener_; }
-    ResponseQueue& GetQueue() { return response_queue_; }
-};
-
-template <typename Price = double, typename Qty = double,
-          typename PositionT = aos::impl::NetPositionDefault<Price, Qty>>
-class BybitNetPositionManagerListener {
-    using ResponseQueue = moodycamel::ConcurrentQueue<std::vector<char>>;
-
-  private:
-    boost::asio::thread_pool& thread_pool_;
-    ResponseQueue response_queue_;
-    aos::impl::NetPositionStorageByPairDefault<> position_storage_;
-    aoe::bybit::impl::ExecutionWatcherDefault<common::MemoryPoolThreadSafety,
-                                              PositionT, Price, Qty>
-        execution_watcher_;
-
-    aoe::bybit::impl::execution_response::ListenerDefault<
-        common::MemoryPoolThreadSafety, PositionT>
-        listener_;
-
-  public:
-    BybitNetPositionManagerListener(boost::asio::thread_pool& thread_pool)
-        : thread_pool_(thread_pool),
-          execution_watcher_(position_storage_),
-          listener_(thread_pool_, response_queue_, execution_watcher_, 100) {}
-    aoe::ResponseQueueListenerInterface& GetListener() { return listener_; }
-    ResponseQueue& GetQueue() { return response_queue_; }
-};
-
-class OrderChannelSessionStarter {
-    boost::asio::io_context& context_;
-    boost::asio::steady_timer timer_;
-    aoe::bybit::WebSocketPrivateSessionRWInterface& session_;
-    aoe::bybit::impl::private_channel::PingManager<std::chrono::seconds>
-        ping_manager_;
-    aoe::bybit::impl::CredentialsLoader bybit_credentials_;
-    aoe::bybit::impl::test_net::PrivateSessionSetup private_session_setuper_;
-    aoe::bybit::impl::OrderSubscriptionBuilder order_subscription_builder_;
-    std::jthread thread_;
-
-  public:
-    OrderChannelSessionStarter(
-        boost::asio::io_context& context,
-        aoe::bybit::WebSocketPrivateSessionRWInterface& session,
-        std::string config_path)
-        : context_(context),
-          timer_(context_),
-          session_(session),
-          ping_manager_(timer_, session_, std::chrono::seconds(20)),
-          bybit_credentials_(config_path),
-          private_session_setuper_(session_, bybit_credentials_, ping_manager_),
-          order_subscription_builder_(session_) {}
-    void Run() {
-        auto status = private_session_setuper_.Setup();
-        if (!status) return;
-        order_subscription_builder_.Subscribe();
-        thread_ = std::jthread([this]() { context_.run(); });
-    }
-};
-
-class ExecutionChannelSessionStarter {
-    boost::asio::io_context& context_;
-    boost::asio::steady_timer timer_;
-    aoe::bybit::WebSocketPrivateSessionRWInterface& session_;
-    aoe::bybit::impl::private_channel::PingManager<std::chrono::seconds>
-        ping_manager_;
-    aoe::bybit::impl::CredentialsLoader bybit_credentials_;
-    aoe::bybit::impl::test_net::PrivateSessionSetup private_session_setuper_;
-    aoe::bybit::impl::ExecutionSubscriptionBuilder
-        execution_subscription_builder_;
-    std::jthread thread_;
-
-  public:
-    ExecutionChannelSessionStarter(
-        boost::asio::io_context& context,
-        aoe::bybit::WebSocketPrivateSessionRWInterface& session,
-        std::string config_path)
-        : context_(context),
-          timer_(context_),
-          session_(session),
-          ping_manager_(timer_, session_, std::chrono::seconds(20)),
-          bybit_credentials_(config_path),
-          private_session_setuper_(session_, bybit_credentials_, ping_manager_),
-          execution_subscription_builder_(session_) {
-        private_session_setuper_.Setup();
-        execution_subscription_builder_.Subscribe();
-    }
-    void Run() {
-        thread_ = std::jthread([this]() { context_.run(); });
-    }
-};
 
 int main(int argc, char** argv) {
     {
@@ -159,33 +19,25 @@ int main(int argc, char** argv) {
         LogPolling log_polling(thread_pool, std::chrono::microseconds(1));
 
         //-------------------------------------------------------------------------------
-        boost::asio::io_context ioc_trade_channel;
-        aoe::bybit::impl::test_net::trade_channel::SessionW
-            session_trade_channel(ioc_trade_channel);
-        BybitOrderManagerListener order_manager(thread_pool,
-                                                session_trade_channel);
-        //-------------------------------------------------------------------------------
-        boost::asio::io_context ioc_private_channel1;
-        aoe::bybit::impl::test_net::private_channel::SessionRW
-            session_private_channel1(ioc_private_channel1,
-                                     order_manager.GetQueue(),
-                                     order_manager.GetListener());
+        moodycamel::ConcurrentQueue<std::vector<char>> queue;
+        aoe::bybit::impl::order_book_response::Listener<
+            common::MemoryPoolThreadSafety>
+            listener(thread_pool, queue);
 
-        std::string config_path = argv[1];
-        OrderChannelSessionStarter order_starter(
-            ioc_private_channel1, session_private_channel1, config_path);
-        order_starter.Run();
         //-------------------------------------------------------------------------------
-        BybitNetPositionManagerListener net_position_manager(thread_pool);
-        //-------------------------------------------------------------------------------
-        boost::asio::io_context ioc_private_channel2;
-        aoe::bybit::impl::test_net::private_channel::SessionRW
-            session_private_channel2(ioc_private_channel2,
-                                     net_position_manager.GetQueue(),
-                                     net_position_manager.GetListener());
-        ExecutionChannelSessionStarter execution_starter(
-            ioc_private_channel2, session_private_channel2, config_path);
-        execution_starter.Run();
+
+        boost::asio::io_context ioc_order_book_channel;
+        aoe::bybit::impl::test_net::spot::order_book_channel::SessionRW
+            session_trade_channel(ioc_order_book_channel, queue, listener);
+        aos::impl::TradingPairPrinter printer;
+        aoe::bybit::impl::spot::OrderBookSubscriptionBuilder sub_builder{
+            session_trade_channel,
+            aoe::bybit::spot::Depth::k50,
+            {2, 1},
+            printer};
+        sub_builder.Subscribe();
+        auto thread_ = std::jthread(
+            [&ioc_order_book_channel]() { ioc_order_book_channel.run(); });
     }
     fmtlog::poll();
     return 0;
